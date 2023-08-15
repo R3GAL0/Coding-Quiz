b26d41aedc2fb0e53f222da2764df11ae9073812
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-08-15 17:00:40 -0400

    added question cycling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,68 @@
-
-
 var questionBank = [
-    "Which of these is not a primitive data type?", 
-    "Which HTML element is used to link the script?", 
-    "Which of these forms a loop?", 
+    "Which of these is not a primitive data type?",
+    "Which HTML element is used to link the script?",
+    "Which of these forms a loop?",
     "What can be stored in an object?"
 ];
 var answerBank = [
-    "Boolean", "String", "Undefined", "Object", 
-    "<script>", "<javascript>", "<js>", "<JSON>", 
-    "if (true)", "while (true)", ".loop", ".itterate", 
+    "Boolean", "String", "Undefined", "Object",
+    "<script>", "<javascript>", "<js>", "<JSON>",
+    "if (true)", "while (true)", ".loop", ".itterate",
     "Primitive data types", "Functions/Methods", "Objects", "All of the above"
 ];
 var solutionBank = ["Object", "<script>", "while (true)", "All of the above"];
 var questionIndex = 0;
 var playerAns = [];
+var startBtn = document.querySelector('#start');
+// timer vars
+var timerEl = document.querySelector("#timer");
+var timeLeft = 10;
 
-// start button
+// when start button clicked: cycle question + hide start button
+startBtn.addEventListener("click", function () {
+    setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = "Time remaining: " + timeLeft + " seconds";
+            timeLeft--;
+        } else if (timeLeft > 0) {
+            timerEl.textContent = "Time remaining: " + timeLeft + " second";
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Game Over";
+            clearInterval();
+            checkResults();
+        }
+    }, 1000);
+    startBtn.textContent = ' ';
+    cycleQ(0);
+})
 
-// cycle question + populate first question
+// cycle question 
+function cycleQ(index) {
+    var h3 = document.querySelector("#question");
+    h3.textContent = questionBank[index];
+    var ulEl = document.querySelector("#question-options");
+    for (var i = index*4; i < 4; i++) {
+        var li = document.createElement("li");
+        li.textContent = answerBank[i];
+        ulEl.appendChild(li);
+    }
+}
 
-// use click on 'start button', 'submit answer/pick answer', 'cycle answer'
 
-window.addEventListener("click", function(){
+window.addEventListener("click", function () {
     // submit answer
     // check answer
 });
 
-// cycle answer
-
 // check answer
-function checkAnswer(){
+function checkAnswer() {
 
 }
 
-var timeLeft = 10;
-var timeInterval = setInterval(function() {
-    if (timeLeft > 1){
-        timerEl.textContent = "Timer: " + timeLeft + "seconds";
-        timeLeft--;
-    }else if (timeLeft > 0) {
-        timerEl.textContent = "Timer: " + timeLeft + "second";
-        timeLeft--;
-    }else {
-        timerEl.textContent = "Game Over";
-        clearInterval(timeInterval);
-        checkResults();
-    }
-}, 1000);
 
 // stops quiz input, tally score, push score to local storage
 function checkResults() {
-    window.removeEventListener();
+    window.removeEventListener("click");
 
 }
